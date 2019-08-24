c0dfd9c5b5ce730e3aac6bc1d764ebf65a880927
lawrayette <milhadasdepapel@gmail.com>
2019-08-24 16:02:14 -0500

    same

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger')
-const navLinks = document.querySelector('.nav-links')
-const links = document.querySelectorAll('.nav-links li')
+const navLinks = document.querySelector('.nav_links')
+const links = document.querySelectorAll('.nav_links li')
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open')
